e3eda21fcdb317eb01291535f8ff81bf2d0374ec
NissiSolution <incrediblenissitest@gmail.com>
2024-12-18 15:38:58 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const app=express()
 const cors=require('cors')
+const app=express()
 const port =3039;
 const router=require('./src/Routes/routes.api')
 app.use(cors({
@@ -10,13 +10,7 @@ app.use(cors({
 app.use( express.json());
 app.use( express.urlencoded( { extended:true } ))
 app.use('/api',router)
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://nissicmms.digidiary.in');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 app.listen(port, () => {
     console.log(`Node serer is listenings on port${port}`);
   });
