8602e8fa18c70c2288c1814658c2201f2b73b3ba
NissiSolution <incrediblenissitest@gmail.com>
2024-12-18 15:15:19 +0530

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ app.use(cors({
 app.use( express.json());
 app.use( express.urlencoded( { extended:true } ))
 app.use('/api',router)
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://nissicmms.digidiary.in');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.listen(port, () => {
     console.log(`Node serer is listenings on port${port}`);
   });
