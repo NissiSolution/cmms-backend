494182cbbcaa2b9ff7be43253ea6ecdafb1028ee
NissiSolution <incrediblenissitest@gmail.com>
2024-12-18 15:02:20 +0530

    first commit

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ const cors=require('cors')
 const port =3039;
 const router=require('./src/Routes/routes.api')
 app.use(cors({
-  origin: 'https://nissicmms.digidiary.in', // Allow requests from specific frontend domain
+  origin: 'https://nissicmms.digidiary.in', 
+  credentials: true,
 }));
-app.use(
-    cors({
-      origin: "*",
-      credentials: true,
-      optionsSuccessStatus: 200,
-    })
-  );
 app.use( express.json());
 app.use( express.urlencoded( { extended:true } ))
-
-
 app.use('/api',router)
 
 app.listen(port, () => {
