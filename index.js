56d2cb71e2c6cdb0edd6e7a4e8e393f7c441a72a
NissiSolution <incrediblenissitest@gmail.com>
2024-12-19 10:38:55 +0530

    changeinindex.js

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const port =3039;
 const router=require('./src/Routes/routes.api')
 app.use(cors({
   origin: (origin, callback) => {
-    const allowedOrigins = ['https://nissicmms.digidiary.in']; // Add allowed origins
-    if (allowedOrigins.includes(origin)) {
+    const allowedOrigins = ['https://nissicmms.digidiary.in', 'http://localhost:3000'];
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
 }));
 
+app.use(express.static('public'));
 app.use( express.json());
 app.use( express.urlencoded( { extended:true } ))
 app.use('/api',router)
