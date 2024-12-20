30315b950d4a5e6e28c556651820e7210a9a92b4
NissiSolution <incrediblenissitest@gmail.com>
2024-12-20 09:49:18 +0530

    add employee

---
+++ src/Controller/employee.controller.js
@@ -0,0 +1,24 @@
+const connection=require('../config/connect')
+
+
+exports.getEmp=(req,res)=>{
+    let query='select * from employee'
+    connection.query(query,(err,result)=>{
+        if(err) throw err;
+        res.status(200).json(result)
+    })
+}
+
+exports.setEmp=(req,res)=>{
+    const {name,department,skillset,email,phone}=req.body;
+    
+    const query ='INSERT INTO employee (name,department,skillset,email,phone) values(?,?,?,?,?)'
+    const data=[name,department,skillset,email,phone];
+    connection.query(query,data,(err,result)=>{
+        if (err) {
+            // Handle error (e.g., duplicate entry, validation issues, etc.)
+            return res.status(500).json({ error: 'Error creating user', details: err });
+        }
+        res.status(201).json({ message: 'User created successfully', result });
+    })
+}
--- src/Routes/routes.api.js
+++ src/Routes/routes.api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const user=require('../Controller/user.controller')
-
+const emp=require('../Controller/employee.controller')
 router.get('/get',user.getUser)
 router.post('/post',user.setUser)
-
+//employee
+router.get('/emp/get',emp.getEmp)
+router.post('/emp/post',emp.setEmp)
 
 module.exports=router
