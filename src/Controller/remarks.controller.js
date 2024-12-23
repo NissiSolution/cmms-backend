3b13c229c27f678091443530828801b43f854a86
NissiSolution <incrediblenissitest@gmail.com>
2024-12-23 16:49:35 +0530

    remarks in remarks

--- src/Controller/remarks.controller.js
+++ src/Controller/remarks.controller.js
@@ -9,7 +9,7 @@ exports.getRemarks=(req,res)=>{
 
 exports.addRemarks=(req,res)=>{
     const {asset_id,remark}=req.body
-    const query='insert into remarks (asset_id,remarks) values(?,?)'
+    const query='insert into remarks (asset_id,remark) values(?,?)'
     const data=[asset_id,remark]
     connection.query(query, data, (err, result) => {
         if (err) {
