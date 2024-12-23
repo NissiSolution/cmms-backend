560db3c813f0ecb9f6df8f0417c59394b8083ae6
NissiSolution <incrediblenissitest@gmail.com>
2024-12-23 17:08:32 +0530

    remarks in remarks

--- src/Controller/remarks.controller.js
+++ src/Controller/remarks.controller.js
@@ -8,9 +8,9 @@ exports.getRemarks=(req,res)=>{
 }
 
 exports.addRemarks=(req,res)=>{
-    const {asset_id,remark}=req.body
-    const query='insert into remarks (asset_id,remark) values(?,?)'
-    const data=[asset_id,remark]
+    const {asset_id,remark,stock}=req.body
+    const query='insert into remarks (asset_id,remark,stock) values(?,?)'
+    const data=[asset_id,remark,stock]
     connection.query(query, data, (err, result) => {
         if (err) {
             return res.status(500).json({ error: 'Failed to create work order', details: err });
